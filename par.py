42c36e14a27623a339fd38f4e4124be7395ad882
Diego Serra <diego.serra@gmail.com>
2024-05-28 21:25:15 -0300

    se agrega validacion de numero positivo

--- par.py
+++ par.py
@@ -3,6 +3,10 @@ def es_par(num):
 
 num = int(input('Ingrese numero:'))
 
+while (num<=0):
+    print('El numero ingresado debe ser mayor a cero')
+    num = int(input('Ingrese numero:'))
+
 if es_par(num):
     print('El numero ingresado es par')
 else:
